3d30c3123a72e880c0f364dead5504d5cb682733
alimohammad0816 <alimohammad0816@gmail.com>
2021-11-29 11:28:05 +0330

    tree

--- tree.py
+++ tree.py
@@ -6,24 +6,14 @@ class BinaryTree:
 
 
 def tree_tracker(tree: BinaryTree):
-    my_tree = tree
-
-    while my_tree:
-        if any((isinstance(value, BinaryTree) for value in my_tree.__dict__.values())):
-            for i in my_tree.__dict__.values():
-                if isinstance(i, BinaryTree):
-                    print(i)
-                    my_tree = i
-
-                else:
-                    print(i)
+    for i in tree.__dict__.values():
+        if isinstance(i, BinaryTree):
+            tree_tracker(i)
 
         else:
-            for i in my_tree.__dict__.values():
-                print(i)
-            break
+            print(i)
 
 
-tree = BinaryTree(3, 4, BinaryTree(5, BinaryTree(6, 7, 8), BinaryTree(9, 10, 11)))
+tree = BinaryTree(3, BinaryTree(1, {"1", 2, 3}, 3), BinaryTree(5, BinaryTree(6, [1, 2], 8), BinaryTree(9, 10, 11)))
 
 tree_tracker(tree)
