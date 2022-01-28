04791fcc58c93d2f30fda8ee0fb6cbd9efdf823c
alimohammad0816 <alimohammad0816@gmail.com>
2022-01-28 23:42:58 +0330

    pan_digit modified

--- pan_digits.py
+++ pan_digits.py
@@ -11,16 +11,27 @@ def is_pan_digit(value: str, pan_digit: str) -> bool:
 
 def find_pandigits():
     p_sum = 0
-
-    for i in range(1, 100):
-        for l in range(1, 2000):
+    for i in range(1, 10):
+        for l in range(1000, 10000):
+            """
+            x * yyyy = zzzz to zzzzz
+            """
             x = f"{i}{l}{i * l}"
             if len(x) == 9:
                 if is_pan_digit(x, "123456789"):
                     print(f"{i} * {l} = {i * l}")
                     p_sum += i * l
 
-    return p_sum
+    for i in range(10, 100):
+        for l in range(100, 1000):
+            """
+            xx * yyy = zzz to zzzzz
+            """
+            x = f"{i}{l}{i * l}"
+            if len(x) == 9:
+                if is_pan_digit(x, "123456789"):
+                    print(f"{i} * {l} = {i * l}")
+                    p_sum += i * l
 
 
 find_pandigits()
